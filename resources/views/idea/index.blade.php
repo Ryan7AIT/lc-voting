29f023e0b6aa8460335b18d62ab9340d02099558
Ryan7AIT <aitmechedalryan7@gmail.com>
2022-01-26 12:46:45 +0100

    add some js to click the card

--- resources/views/idea/index.blade.php
+++ resources/views/idea/index.blade.php
@@ -28,7 +28,15 @@
 
     <div class="ideas-container space-y-6 my-8">
         @foreach ($ideas as $idea)
-            <div class="idea-container hover:shadow-card transition duration-150 ease-in bg-white rounded-xl flex cursor-pointer">
+            <div
+            x-data
+            @click="const target = $event.target.tagName.toLowerCase()
+                const ignores = ['button','svg','path','a', 'img']
+                const ideaLink = $event.target.closest('.idea-container').querySelector('.idea-link')
+                
+                !ignores.includes(target) && ideaLink.click()
+            "
+            class="idea-container hover:shadow-card transition duration-150 ease-in bg-white rounded-xl flex cursor-pointer">
                 <div class="hidden md:block border-r border-gray-100 px-5 py-8">
                     <div class="text-center">
                         <div class="font-semibold text-2xl">12</div>
@@ -47,7 +55,7 @@
                     </div>
                     <div class="w-full flex flex-col justify-between mx-2 md:mx-4">
                         <h4 class="text-xl font-semibold mt-2 md:mt-0">
-                            <a href="{{ route('idea.show', $idea) }}" class="hover:underline"> 
+                            <a href="{{ route('idea.show', $idea) }}" class="idea-link hover:underline">
 
                                 {{ $idea->title }}
 
